61275f0494ba7945b4624209b6ab0f22d976f99c
jorge <jherpue553@g.educaand.es>
2022-11-17 10:26:41 +0100

    Termino la tarea

--- src/main/java/clases/Prueba.java
+++ src/main/java/clases/Prueba.java
@@ -30,14 +30,14 @@ public class Prueba {
         float imcKosio = kosio.calcularIMC();
         float imcVacia = personaVacia.calcularIMC();
         if (imcKosio< 18.5){
-            tipoPesoKosio = "inferio al normal";
+            tipoPesoKosio = "inferior al normal";
         } else if (imcKosio <= 24.9) {
             tipoPesoKosio = "normal";
         }else if (imcKosio > 24.9) {
             tipoPesoKosio = "superior al normal";
         }
         if (imcVacia< 18.5){
-            tipoPesoVacia = "inferio al normal";
+            tipoPesoVacia = "inferior al normal";
         } else if (imcVacia <= 24.9) {
             tipoPesoVacia = "normal";
         }else if (imcVacia > 24.9) {
@@ -47,7 +47,15 @@ public class Prueba {
                 " por lo que su peso es " + tipoPesoKosio);
         System.out.println("El IMC de persona vacía es " + personaVacia.calcularIMC()
                 + " por lo que su peso es " + tipoPesoVacia);
-        // Digo si 
+        // Creo un objeto tipo Serie
+        Serie house = new Serie("House", "Medicina/Drama", 
+                "Un doctor siempre tiene razón", "BBC", 8, 9999);
+        // Enseño los datos de la serie para comparar luego
+        System.out.println(house);
+        // Hago que kosio le de "me gusta" a "house"
+        kosio.darLikeSerie(house);
+        // Vuelvo a enseñar los datos de "house" para ver si le ha dado like
+        System.out.println(house);
         
     }
 }
